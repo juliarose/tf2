1855b61c6d31159523f29203ae252208d55334ae
juliarose <julialunarose@gmail.com>
2022-04-04 17:25:08 -0400

    added tf2-protobuf

---
+++ tf2-protobuf/README.md
@@ -0,0 +1,9 @@
+# tf2-protobuf
+
+Team Fortress 2 Game Coordinator protobuf messages.
+
+## Building
+
+- install `protobuf-compiler` from your package manager
+- install `protoc-gen-rust` using `cargo install protobuf-codegen`
+- run `./build.sh`
---
+++ tf2-protobuf/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod base_gcmessages;
+pub mod econ_gcmessages;
+pub mod gcsdk_gcmessages;
+pub mod gcsystemmsgs;
+pub mod steamdatagram_auth_messages;
+pub mod steamdatagram_messages;
+pub mod steammessages;
+pub mod tf_gcmessages;
+pub mod tf_proto_def_messages;
